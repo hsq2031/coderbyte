bb0775b69e98e36f962d09f051a8a316afc37a8e
hsq2031 <hsq2031@gmail.com>
2015-07-31 13:40:36 +0900

    Create Palindrome.rb

---
+++ Palindrome.rb
@@ -0,0 +1,12 @@
+def Palindrome(str)
+
+  # code goes here
+ 
+  str = str.split(' ').join('')
+  return str == str.reverse
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+Palindrome(STDIN.gets)           
